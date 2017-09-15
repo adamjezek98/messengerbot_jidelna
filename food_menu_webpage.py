ff20ffa52e3293de4fe4927c60edf3edf7e19c62
Adam Ježek <mail@ajezek.cz>
2017-09-15 22:55:32 +0200

    single page menu in separate file

---
+++ food_menu_webpage.py
@@ -0,0 +1,31 @@
+import http.server
+import sqlite3
+import datetime
+import botconfig
+import os
+
+os.chdir(botconfig.home_folder)
+
+class WebHandler(http.server.SimpleHTTPRequestHandler):
+    # def log_message(self, format, *args):
+    #    return
+
+
+    def do_GET(self):
+        result = "<meta charset='UTF-8'><h1>Jídelní lístek</h1><br/>"  # <h2>Školní jídelna Gymázium Roudnice nad Labem<h2/>"
+        db = sqlite3.connect("database.sqlite3")
+        c = db.cursor()
+        c.execute("SELECT * FROM foods WHERE date >= julianday(?) ORDER BY date;",
+                  ([datetime.date.today().strftime("%Y-%m-%d")]))
+        result += '<table style="text-align: left;" border="1">'
+        for r in c.fetchall():
+            result += "<tr><th>" + r[2] + "</th><th>" + r[3].replace("\n", "<br/>") + "</th></tr>"
+        result += "</table>"
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        self.wfile.write(bytes(str(result), "utf-8"))
+
+
+server = http.server.HTTPServer(("0.0.0.0", 8000), WebHandler)
+server.serve_forever()
