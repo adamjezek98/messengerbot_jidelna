41aaff31b43d70d3b19b0f9487e0f2138cd71ee9
Adam Ježek <mail@ajezek.cz>
2017-09-27 21:32:45 +0200

    better html, GA

--- food_menu_webpage.py
+++ food_menu_webpage.py
@@ -14,13 +14,27 @@ class WebHandler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
         result = """
         <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
-<html>
-  <head><meta charset='UTF-8'>
+<html lang="cs">
+  <head>
+  
+  <meta http-equiv="content-type" content="text/html; charset=utf-8">
+    <title>Jídelní lístek - Gymnázium Roudnice</title>
+    <meta charset='UTF-8'>
         <meta property="og:url"                content="http://ajezek.cz:8000" />
         <meta property="og:type"               content="article" />
         <meta property="og:title"              content="Jídelní lístek" />
         <meta property="og:description"        content="Školní jídelna Gymnázium Roudnice" />
         <meta property="og:image"              content="https://ajezek.cz/images/icon_jidelna.png" />
+        <!-- Global Site Tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-106889531-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments)};
+  gtag('js', new Date());
+
+  gtag('config', 'UA-106889531-1');
+</script>
+
         </head><body>
         <h1>Jídelní lístek</h1><br/>"""  # <h2>Školní jídelna Gymázium Roudnice nad Labem<h2/>"
         db = sqlite3.connect("database.sqlite3")
@@ -31,6 +45,7 @@ class WebHandler(http.server.SimpleHTTPRequestHandler):
         for r in c.fetchall():
             result += "<tr><th>" + r[2] + "</th><th>" + r[3].replace("\n", "<br/>") + "</th></tr>"
         result += """</table>
+       
         </body>
         </html>
 """
